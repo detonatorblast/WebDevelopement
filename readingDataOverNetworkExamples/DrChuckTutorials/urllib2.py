3137a426fa72bbc59533103fdb6abceb5724e9e4
muktesh mukul <mukteshmukul@gmail.com>
2019-11-14 14:42:47 +0530

    deleting dr chuck tutorials

--- readingDataOverNetworkExamples/DrChuckTutorials/urllib2.py
+++
@@ -1,5 +0,0 @@
-import urllib.request, urllib.parse, urllib.error
-
-fhand = urllib.request.urlopen('http://py4e-data.dr-chuck.net/comments_42.html')
-for line in fhand:
-    print(line.decode().strip())
